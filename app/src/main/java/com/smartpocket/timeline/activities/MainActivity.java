93965f66c4b181fbb0d5fead1105f445ee112735
Esteban <estebanlagord@gmail.com>
2016-04-17 14:02:50 -0300

    Navigation Drawer finished

--- app/src/main/java/com/smartpocket/timeline/activities/MainActivity.java
+++ app/src/main/java/com/smartpocket/timeline/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.smartpocket.timeline.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,9 +17,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
 import com.facebook.Profile;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.facebook.login.widget.ProfilePictureView;
 import com.smartpocket.timeline.R;
 import com.smartpocket.timeline.adapter.PostAdapter;
@@ -26,13 +36,18 @@ import com.smartpocket.timeline.backend.ServiceHandler;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private RecyclerView mRecyclerView;
+    private NavigationView mNavigationView;
     private PostAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    SwipeRefreshLayout mSwipeRefreshLayout;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -40,28 +55,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.timeline_recycler_view);
-        // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        // specify an adapter
+        // specify an adapter for RecyclerView
         mAdapter = new PostAdapter();
         mRecyclerView.setAdapter(mAdapter);
         ServiceHandler.getInstance().setAdapter((PostAdapter) mRecyclerView.getAdapter());
-
-        AccessToken accessToken = AccessToken.getCurrentAccessToken();
-        ProfilePictureView myPicture = (ProfilePictureView) navigationView.getHeaderView(0).findViewById(R.id.myProfilePicture);
-        myPicture.setProfileId(accessToken.getUserId());
-
-        TextView myUserNameView = (TextView) navigationView.getHeaderView(0).findViewById(R.id.myUserName);
-        myUserNameView.setText(Profile.getCurrentProfile().getName());
 
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.activity_main_swipe_refresh_layout);
         mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -71,7 +78,68 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
+        // Facebook setup
+        callbackManager = CallbackManager.Factory.create();
+        AppEventsLogger.activateApp(getApplication());
+
+        LoginButton loginButton = (LoginButton) mNavigationView.getMenu().getItem(0).getActionView().findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_posts");
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(MainActivity.this, getString(R.string.login_success), Toast.LENGTH_SHORT).show();
+                //TODO close nav drawer
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(MainActivity.this, getString(R.string.login_cancel), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Toast.makeText(MainActivity.this, getString(R.string.login_error), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Track user logging in/out of Facebook
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                updateUserInfo();
+            }
+        };
+
+        updateUserInfo();
+    }
+
+    private void updateUserInfo(){
+        ProfilePictureView myPicture = (ProfilePictureView) mNavigationView.getHeaderView(0).findViewById(R.id.myProfilePicture);
+        TextView myUserNameView = (TextView) mNavigationView.getHeaderView(0).findViewById(R.id.myUserName);
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+
+        if (accessToken != null) {
+            // User is logged in
+            myPicture.setProfileId(accessToken.getUserId());
+            myUserNameView.setText(Profile.getCurrentProfile().getName());
+        } else {
+            // user is logged out
+            myPicture.setProfileId(null);
+            myUserNameView.setText(getResources().getString(R.string.please_sign_in));
+        }
+
+        invalidateOptionsMenu();
         refreshContents();
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -101,6 +169,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        if (accessToken == null) {
+            // user is logged out, disable refresh
+            menu.findItem(R.id.action_refresh).setVisible(false);
+        }
+
         return true;
     }
 
@@ -116,23 +191,32 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return super.onOptionsItemSelected(item);
     }
 
-    protected void onResume() {
-        super.onResume();
-        //refreshContents();
-    }
-
     private void refreshContents() {
-        mSwipeRefreshLayout.setRefreshing(true);
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
         mAdapter.clear();
-        ServiceHandler.getInstance().getUserFeed(true);
-        // after the user feed is refreshed, disable the refresh state for the swipe refresh layout
-        mAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-            @Override
-            public void onItemRangeInserted(int positionStart, int itemCount) {
-                super.onItemRangeInserted(positionStart, itemCount);
-                mSwipeRefreshLayout.setRefreshing(false);
-            }
 
-        });
+        if (accessToken != null) {
+            // user is logged in
+            mSwipeRefreshLayout.setRefreshing(true);
+
+            ServiceHandler.getInstance().getUserFeed(true);
+            // after the user feed is refreshed, disable the refresh state for the swipe refresh layout
+            mAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+                @Override
+                public void onItemRangeInserted(int positionStart, int itemCount) {
+                    super.onItemRangeInserted(positionStart, itemCount);
+                    mSwipeRefreshLayout.setRefreshing(false);
+                }
+
+            });
+        }
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (accessTokenTracker != null)
+            accessTokenTracker.stopTracking();
+    }
+
 }
